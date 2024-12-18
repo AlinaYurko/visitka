71b2ee3501233b5f3c6ed8678e4376596f0bf811
AlinaYurko <alinaurko35@gmail.com>
2024-12-18 18:59:52 +0300

    main

--- java.js
+++ java.js
@@ -1,16 +1,16 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const burger = document.getElementById('burger');
-    const navLinks = document.getElementById('nav-links');
+// document.addEventListener('DOMContentLoaded', () => {
+//     const burger = document.getElementById('burger');
+//     const navLinks = document.getElementById('nav-links');
 
-    burger.addEventListener('click', () => {
-        navLinks.classList.toggle('active');
-    });
+//     burger.addEventListener('click', () => {
+//         navLinks.classList.toggle('active');
+//     });
 
-    // Обработчики для навигационных ссылок (если нужно)
-    const links = document.querySelectorAll('.nav-links a');
-    links.forEach(link => {
-        link.addEventListener('click', () => {
-            navLinks.classList.remove('active'); // Закрыть меню при выборе ссылки
-        });
-    });
-});
+//     // Обработчики для навигационных ссылок (если нужно)
+//     const links = document.querySelectorAll('.nav-links a');
+//     links.forEach(link => {
+//         link.addEventListener('click', () => {
+//             navLinks.classList.remove('active'); // Закрыть меню при выборе ссылки
+//         });
+//     });
+// });
